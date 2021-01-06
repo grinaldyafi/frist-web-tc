31a966ed1dc734cc3b9747e0062fca6ce5e4c61c
grinaldyafi <grinaldy.yafi@mail.ugm.ac.id>
2021-01-06 11:13:09 +0700

    ubah logo dan src

--- js_scripts.js
+++ js_scripts.js
@@ -1,13 +1,14 @@
-<script type="text/javascript">
-var nav =document.getElementById('nav');
-window.onscroll = function () {
-    if(window.pageYOfset > 100){
+< script type = "text/javascript" >
+    var nav = document.getElementById('nav');
+window.onscroll = function() {
+    if (window.pageYOfset > 100) {
         nav.style.position = "fixed";
         nav.style.top = 0;
-
-    }else{
-        box.style.position ="absolute";
-       box.style.top = 100;
-    } 
+    }
+} else {
+    box.style.position = "absolute";
+    box.style.top = 100;
 }
-</script>
+
+<
+/script>
